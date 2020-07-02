bf255be0407510e43053debab5dc78f12f506f8a
Jhoan Montoya <jhoanm09@gmail.com>
2020-07-01 21:44:36 -0500

    Se agrega AutoPause y se elimina AutoMute

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,21 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
-import AutoMute from './plugins/AutoMute.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoMute()] });
+const player = new MediaPlayer({ 
+    el: video,
+    plugins: [new AutoPlay(), new AutoPause()]
+});
 
-const buttonPlay = document.getElementById('play');
+const buttonPlay = document.querySelector('#playButton');
 buttonPlay.onclick = () => player.togglePlay();
-const buttonSound = document.getElementById('sound');
-buttonSound.onclick = () => player.toggleSound();
+
+const buttonSound = document.querySelector('#muteButton');
+buttonSound.onclick = () => {
+    if(player.media.muted) {
+        player.unmute(); 
+    } else {
+        player.muted();
+    }
+} 
--- assets/plugins/AutoMute.js
+++
@@ -1,9 +0,0 @@
-function AutoMute() {}
-
-AutoMute.prototype.run = function(player) {
-    if(!player.muted) {
-        player.muted = true 
-    }
-}
-
-export default AutoMute
